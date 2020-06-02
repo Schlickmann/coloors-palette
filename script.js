2a254eff606a2f2c8cc85d42c77825b17246f32a
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-06-02 08:49:22 -0700

    dynamically update sliders color

--- script.js
+++ script.js
@@ -50,6 +50,9 @@ function randomColors() {
 
     colorizeSliders(color, hue, brightness, saturation);
   });
+
+  // reset inputs
+  resetInputs();
 }
 
 function checkTextContrast(color, text) {
@@ -105,6 +108,9 @@ function hslControls(event) {
     .set("hsl.h", hue.value);
 
   colorDivs[index].style.backgroundColor = color;
+
+  // colorize sliders
+  colorizeSliders(color, hue, brightness, saturation);
 }
 
 function updateTextUI(index) {
@@ -121,6 +127,34 @@ function updateTextUI(index) {
   for (icon of icons) {
     checkTextContrast(color, icon);
   }
+}
+
+function resetInputs() {
+  const sliders = document.querySelectorAll(".sliders input");
+  sliders.forEach((slider) => {
+    if (slider.name === "hue") {
+      const hueColor = initialColors[slider.getAttribute("data-hue")];
+      const hueValue = chroma(hueColor).hsl()[0];
+
+      slider.value = Math.floor(hueValue);
+    }
+
+    if (slider.name === "saturation") {
+      const saturationColor =
+        initialColors[slider.getAttribute("data-saturation")];
+      const saturationValue = chroma(saturationColor).hsl()[1];
+
+      slider.value = Math.floor(saturationValue * 100) / 100;
+    }
+
+    if (slider.name === "brightness") {
+      const brightnessColor =
+        initialColors[slider.getAttribute("data-brightness")];
+      const brightnessValue = chroma(brightnessColor).hsl()[2];
+
+      slider.value = Math.floor(brightnessValue * 100) / 100;
+    }
+  });
 }
 
 randomColors();
