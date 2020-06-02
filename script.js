6e78ddae242c1a2468d9f87768404dcfe1d0b42b
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-06-01 19:22:32 -0700

    added chroma js to generate colors

---
+++ script.js
@@ -0,0 +1,29 @@
+// Global selections and variables
+const colorDivs = document.querySelectorAll(".color");
+const generateButton = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll(".color h2");
+
+let initialColors;
+
+// Functions
+
+// Color generator
+function generateHex() {
+  const hexColor = chroma.random();
+
+  return hexColor;
+}
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+
+    // Add color to background
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+  });
+}
+
+randomColors();
